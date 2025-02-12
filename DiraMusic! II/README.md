8eb69a8361694dd0d139710468bb8ec2be2b767e
Diramix <danyagranny10@proton.me>
2025-02-12 04:28:36 +0300

    Migration to PulseSync 2.0.0

---
+++ DiraMusic! II/README.md
@@ -0,0 +1,7 @@
+![2024-08-30_223011](https://github.com/user-attachments/assets/3c26afbf-826d-4af3-a3b6-f921310c94da)
+   
+# DiraMusic! II
+
+## Ссылки
+**Boosty:** https://boosty.to/diramix
+**Discord Server:** https://discord.gg/ky6bcdy7KA
--- DiraMusic! II/script.js
+++ DiraMusic! II/script.js
@@ -1,3 +1,5 @@
+// Автоскрытие пинлиста
+/*--------------------------------------------*/
 setInterval(() => {
     const pinsContent = document.querySelector('.PinsList_content__9RG7s');
     const layoutContent = document.querySelector('.DefaultLayout_content__md70Z');
@@ -5,4 +7,14 @@ setInterval(() => {
     if (pinsContent && layoutContent) {
         layoutContent.style.right = pinsContent.children.length === 0 ? '8px' : '65px';
     }
-}, 1000);
+
+// Улучшение разрешения картинки плеера
+    const imgElements = document.querySelectorAll('[class*="PlayerBarDesktop_cover__IYLwR"]');
+
+    imgElements.forEach(img => {
+        if (img.src && img.src.includes('/100x100')) {
+            img.srcset = img.srcset.replace(/\/100x100/g, '/1000x1000'); // Нормальное разрешение картинки плеера
+        }
+    });
+}, 100);
+/*--------------------------------------------*/
